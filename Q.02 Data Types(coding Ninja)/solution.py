24677ff277e530794f7bf5179b0890228a0df21a
Atulit95 <atulitgupta436@gmail.com>
2024-04-18 00:59:54 +0530

    Q2

---
+++ Q.02 Data Types(coding Ninja)/solution.py
@@ -0,0 +1,10 @@
+from typing import *
+
+
+def dataTypes(type: str):
+    if type == "Integer" or type == "Float":
+        return 4
+    elif type == "Long" or type == "Double":
+        return 8
+    else:
+        return 1
