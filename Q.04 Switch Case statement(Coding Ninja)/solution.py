722d2910bfd30bc26bb0bea557434774e8761053
Atulit95 <atulitgupta436@gmail.com>
2024-04-18 11:19:22 +0530

    Q3

---
+++ Q.04 Switch Case statement(Coding Ninja)/solution.py
@@ -0,0 +1,16 @@
+from typing import *
+import math
+
+
+def areaSwitchCase(ch: int, a: List[float]):
+    # Write your code here
+    area = 0.0
+
+    # Using if-elif-else statements to calculate the area based on the shape.
+    if ch == 1:  # Circle
+        area = math.pi * a[0] * a[0]
+    elif ch == 2:  # Rectangle
+        area = a[0] * a[1]
+
+    # Returning the calculated area upto 5 decimal places.
+    return "{:.5f}".format(area)
