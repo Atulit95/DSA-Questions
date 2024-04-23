239f90c50c7604264d18a0091565f364eb2dff4c
Atulit95 <atulitgupta436@gmail.com>
2024-04-23 23:27:23 +0530

    Q5

---
+++ Q.05 Nth Fibonacci Number(Coding Ninja)/Nth_Fibonacci_Number.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Nth_Fibonacci_Number {
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+
+		int first_element=1;
+		int next_element = 1;
+		int third_element = first_element + next_element;
+		
+		if (n == 1) {
+			System.out.println(first_element);
+		}
+		else if (n == 2) {
+			System.out.println(next_element);
+		}
+		else {
+			for (int i = 1; i <= n-2; i++) {
+				third_element = first_element + next_element;
+				first_element = next_element;
+				next_element = third_element;
+			}
+			System.out.println(third_element);
+		}
+		sc.close();
+	}
+
+}
