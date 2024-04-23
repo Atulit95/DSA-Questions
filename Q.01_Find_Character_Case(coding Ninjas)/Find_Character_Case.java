5a7c182ac60135b0c4ff6627223b561a2e77bc92
Atulit95 <atulitgupta436@gmail.com>
2024-04-23 23:22:55 +0530

    Q1

---
+++ Q.01_Find_Character_Case(coding Ninjas)/Find_Character_Case.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Find_Character_Case {
+    
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner sc =new Scanner(System.in);
+        int user_input=sc.next().charAt(0);
+
+        if( user_input>=97 && user_input<122 ){
+            System.out.println(0);
+        }
+        else if( user_input>=65 && user_input<90 ){
+            System.out.println(1);
+        }
+        else {
+            System.out.println(-1);
+        }
+        sc.close();
+ 
+    }
+}
