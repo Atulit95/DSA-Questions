daa45f6bebaebf430177b66bdb55cef0d53e8fea
Atulit95 <atulitgupta436@gmail.com>
2024-04-23 23:27:29 +0530

    de

---
+++ Q.06 Sum of even & odd/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    	
+	public static void main(String[] args) {
+		// Write your code here
+        Scanner sc=new Scanner(System.in);
+        int input=sc.nextInt();
+        System.out.println("hello");
+        
+
+	}
+}
