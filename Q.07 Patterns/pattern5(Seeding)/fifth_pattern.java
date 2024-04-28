a415b4d06526a034c786ac1a6a13cde8ce7fb16f
Atulit95 <atulitgupta436@gmail.com>
2024-04-28 21:30:42 +0530

    Q7

--- Q.07 Patterns/pattern5(Seeding)/fifth_pattern.java
+++ Q.07 Patterns/pattern5(Seeding)/fifth_pattern.java
@@ -4,7 +4,6 @@ public class fifth_pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(n);
         for (int i = n; i >= 1; i--) {
             for (int j = i; j >= 1; j--) {
                 System.out.print("* ");
