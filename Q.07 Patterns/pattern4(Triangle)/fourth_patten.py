ff94822b56e88c19354ac4c8f2daf23017d58c57
Atulit95 <atulitgupta436@gmail.com>
2024-04-28 21:19:34 +0530

    Q7

---
+++ Q.07 Patterns/pattern4(Triangle)/fourth_patten.py
@@ -0,0 +1,9 @@
+def Triangle(n: int) -> None:
+    # Write your solution here.
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(i, end=" ")
+        print()
+
+
+print(Triangle(5))
