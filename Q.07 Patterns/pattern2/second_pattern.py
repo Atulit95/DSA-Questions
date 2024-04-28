cfe36602ec0ce05a1a7a54992424a7995c00bf65
Atulit95 <atulitgupta436@gmail.com>
2024-04-28 20:35:53 +0530

    Q7

---
+++ Q.07 Patterns/pattern2/second_pattern.py
@@ -0,0 +1,9 @@
+def nForest(n: int) -> None:
+    # Write your solution here.
+    for i in range(0, n + 1):
+        for j in range(0, i):
+            print("* ", end="")
+        print()
+
+
+print(nForest(5))
