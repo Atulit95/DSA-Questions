28b4cce2fc8f506063a204c100c76c110e97a0d1
Atulit95 <atulitgupta436@gmail.com>
2024-07-12 09:59:40 +0530

    Q15

---
+++ Q.15 Print 1 To N Without Loop(GFG)/PythonSolution.py
@@ -0,0 +1,36 @@
+class Solution:    
+    #Complete this function
+    def printNos(self,N):
+        #Your code here
+        if N == 1:
+            print(1,end=" ")
+        else:
+            Solution.printNos(self,N-1)
+            print(N,end=" ")
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        N=int(input())
+        
+        ob=Solution()
+        
+        ob.printNos(N)
+        print()
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
